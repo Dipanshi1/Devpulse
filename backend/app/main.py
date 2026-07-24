231dca961f6bba56e57dbbadc768219dd660cc3b
Dipanshi1 <dipanshig6969@gmail.com>
2026-07-24 13:52:59 +0530

    feat: add GitHub repository analytics

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,5 @@
+
+
 import httpx
 from fastapi import FastAPI
 
@@ -10,17 +12,28 @@ def root():
 @app.get("/developers/{username}")
 def get_developers(username: str):
     url = f"https://api.github.com/users/{username}"
-    response = httpx.get(url)
-    data = response.json()
+    user_response = httpx.get(url)
+    user_data = user_response.json()
+    repos_url = f"https://api.github.com/users/{username}/repos"
+    total_stars = 0
+    total_forks = 0
+    repos_response = httpx.get(repos_url)
+    repos_data = repos_response.json()
+    for repo in repos_data:
+        total_stars += repo["stargazers_count"]
+        total_forks += repo["forks_count"]
+
     return {
-    "username": data["login"],
-    "name": data["name"],
-    "followers": data["followers"],
-    "following": data["following"],
-    "public_repos": data["public_repos"],
-    "avatar_url": data["avatar_url"],
-    "profile_url": data["html_url"],
-    "bio": data["bio"],
-    "company": data["company"],
-    "location": data["location"],
-}
+        "username": user_data["login"],
+        "name": user_data["name"],
+        "followers": user_data["followers"],
+        "following": user_data["following"],
+        "public_repos": user_data["public_repos"],
+        "avatar_url": user_data["avatar_url"],
+        "profile_url": user_data["html_url"],
+        "bio": user_data["bio"],
+        "company": user_data["company"],
+        "location": user_data["location"],
+        "total_stars": total_stars,
+        "total_forks": total_forks
+    }
