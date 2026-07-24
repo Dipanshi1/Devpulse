eac3819fe40d44e5df02a3a0225087c0cf6f192f
Dipanshi1 <dipanshig6969@gmail.com>
2026-07-24 09:13:06 +0530

    feat: integrate GitHub user profile API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+import httpx
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -8,4 +9,18 @@ def root():
     return {"message": "DevPulse API is running"}
 @app.get("/developers/{username}")
 def get_developers(username: str):
-    return {"message": f"Developer: {username}"}
+    url = f"https://api.github.com/users/{username}"
+    response = httpx.get(url)
+    data = response.json()
+    return {
+    "username": data["login"],
+    "name": data["name"],
+    "followers": data["followers"],
+    "following": data["following"],
+    "public_repos": data["public_repos"],
+    "avatar_url": data["avatar_url"],
+    "profile_url": data["html_url"],
+    "bio": data["bio"],
+    "company": data["company"],
+    "location": data["location"],
+}
