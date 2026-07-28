bc2a425164d28649135e5a9169c2c1f58dc1b33f
Dipanshi1 <dipanshig6969@gmail.com>
2026-07-28 14:52:21 +0530

    feat: enhance developer endpoint with repository statistics and error handling

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,3 @@
-
-
 import httpx
 from fastapi import FastAPI, HTTPException
 
@@ -13,26 +11,33 @@ def root():
 def get_developers(username: str):
     url = f"https://api.github.com/users/{username}"
     user_response = httpx.get(url)
-    if user_response.status_code == 404:
-      raise HTTPException(
-        status_code=404,
-        detail="GitHub user not found"
+    if user_response.status_code != 200:
+     raise HTTPException(
+        status_code=user_response.status_code,
+        detail=user_response.json().get("message")
     )
+    
     user_data = user_response.json()
     repos_url = f"https://api.github.com/users/{username}/repos"
     total_stars = 0
     total_forks = 0
     most_starred_repo = None
     repos_response = httpx.get(repos_url)
+
+    if repos_response.status_code != 200:
+     raise HTTPException(
+        status_code=repos_response.status_code,
+        detail=repos_response.json().get("message")
+    )
     repos_data = repos_response.json()
     for repo in repos_data:
         total_stars += repo["stargazers_count"]
         total_forks += repo["forks_count"]
         if most_starred_repo is None:
          most_starred_repo = repo
-
         elif repo["stargazers_count"] > most_starred_repo["stargazers_count"]:
          most_starred_repo = repo
+         
 
     return {
         "username": user_data["login"],
@@ -47,10 +52,15 @@ def get_developers(username: str):
         "location": user_data["location"],
         "total_stars": total_stars,
         "total_forks": total_forks,
-        "most_starred_repo": {
-         "name": most_starred_repo["name"],
-         "stars": most_starred_repo["stargazers_count"],
-         "url": most_starred_repo["html_url"]
-       }
+        "most_starred_repo": (
+    {
+        "name": most_starred_repo["name"],
+        "stars": most_starred_repo["stargazers_count"],
+        "url": most_starred_repo["html_url"]
     }
+    if most_starred_repo
+    else None
+)
+       }
+    
     
